6e893ad66de144c4f729fd9e4a986300f0462222
arnitdo <arnav.nitin.deo@gmail.com>
2020-07-22 01:46:21 +0530

    Added Sleep timer Function

--- gpguio.py
+++ gpguio.py
@@ -5,10 +5,18 @@ filewrite.write("import time\n")
 filewrite.write("import gpiozero\n")
 
 #windows
-mainwindow = guizero.App()
-LEDwindow = guizero.Window(mainwindow)
+mainwindow = guizero.App(title = "gpguio", width = 640, height = 480)
+LEDwindow = guizero.Window(mainwindow, width = 640, height = 480)
 LEDwindow.hide()
+Sleepwindow = guizero.Window(mainwindow, width = 640, height = 480)
+Sleepwindow.hide()
+Powerwindow = guizero.Window(mainwindow, width = 640, height = 480)
+Powerwindow.hide()
 
+def LEDwindowexit():
+    LEDnamebox.clear()
+    LEDpinnumberbox.clear()
+    LEDwindow.hide()
 def updateLEDname():
     LEDname = LEDnamebox.value
     LEDpinnumber = LEDpinnumberbox.value
@@ -16,24 +24,37 @@ def updateLEDname():
     LEDnamebox.clear()
     LEDpinnumberbox.clear()
     LEDwindow.hide()
-
+    actiontext = guizero.Text(mainwindow, text = "Added LED with name " + str(LEDname) + " at GPIO pin " + str(LEDpinnumber))
+def updatePoweractions():
+    Sleeptime = Sleeptimebox.value
+    filewrite.write("time.sleep(" + str(Sleeptime) + ")\n" )
+    Sleeptimebox.clear()
+    Sleepwindow.hide()
+    actiontext = guizero.Text(mainwindow, text = "Added sleep timer for "+ str(Sleeptime) + " seconds",)
 
 #buttons
 #mainwindow
 LEDselectbutton = guizero.PushButton(mainwindow, command = LEDwindow.show, text = "New LED")
-mainwindowexitbutton = guizero.PushButton(mainwindow, command = mainwindow.destroy, text = "Exit")
+Sleepwindowbutton = guizero.PushButton(mainwindow, command = Sleepwindow.show, text = "Add Sleep Timer")
+Powerwindowbutton = guizero.PushButton(mainwindow, command = Sleepwindow.show, text = "Power Controls")
+mainwindowexitbutton = guizero.PushButton(mainwindow, command = mainwindow.destroy, text = "Exit", align = "bottom")
 #LEDwindow
-LEDselectexitbutton = guizero.PushButton(LEDwindow, command = LEDwindow.hide, text = "Cancel")
-LEDnameconfirmbutton = guizero.PushButton(LEDwindow, command = updateLEDname, text = "Confirm LED selection")
+LEDselectexitbutton = guizero.PushButton(LEDwindow, command = LEDwindowexit, text = "Cancel", align = "bottom")
+LEDnameconfirmbutton = guizero.PushButton(LEDwindow, command = updateLEDname, text = "Confirm LED selection", align = "top")
+
+#Sleepwindow
+Sleeptimeconfirmbutton = guizero.PushButton(Sleepwindow, command = updatePoweractions, text = "Confirm Sleep Timer")
 
 #user input fields
-LEDnametext = guizero.Text(LEDwindow, text="LED name (can be anything)")
+LEDnametext = guizero.Text(LEDwindow, text="\nLED name (can be anything)")
 LEDnamebox = guizero.TextBox(LEDwindow)
-LEDpinnumbertext = guizero.Text(LEDwindow, text = "GPIO pin number to which LED is connected")
+LEDpinnumbertext = guizero.Text(LEDwindow, text = "\nGPIO pin number to which LED is connected.\nShould be any number from 1 to 40")
 LEDpinnumberbox = guizero.TextBox(LEDwindow)
-
+Sleeptimetext = guizero.Text(Sleepwindow, text = "Sleep time (in seconds)")
+Sleeptimebox = guizero.TextBox(Sleepwindow)
 #End of file
 mainwindow.display()
 filewrite.close()
 sys.exit()
+
 
