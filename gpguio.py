5e2ed871481364022bb3627a22a5109fb8b25de0
arnitdo <arnav.nitin.deo@gmail.com>
2020-07-21 23:53:41 +0530

    Added basic LED functionality

--- gpguio.py
+++ gpguio.py
@@ -1,19 +1,35 @@
-#imports
 import guizero
-#import gpiozero
 filewrite = open("script.py", "w")
-
-def LEDwindowshow():
-    LEDwindow.show()
+filewrite.write("import time\n")
+filewrite.write("import gpiozero\n")
+#windows
 mainwindow = guizero.App()
 LEDwindow = guizero.Window(mainwindow)
 LEDwindow.hide()
 
-def killLEDwindow():
+def updateLEDname():
+    LEDname = LEDnamebox.value
+    LEDpinnumber = LEDpinnumberbox.value
+    filewrite.write(str(LEDname) + " = LED(" + str(LEDpinnumber) + ")\n")
+    LEDnamebox.clear
+    LEDpinnumberbox.clear
     LEDwindow.hide()
-LEDselectbutton = guizero.PushButton(mainwindow, command = LEDwindowshow, args = None, text = "New LED")
-LEDselectexitbutton = guizero.PushButton(LEDwindow, command = killLEDwindow, args = None, text = "Exit")
 
+
+#buttons
+#mainwindow
+LEDselectbutton = guizero.PushButton(mainwindow, command = LEDwindow.show, text = "New LED")
+mainwindowexitbutton = guizero.PushButton(mainwindow, command = mainwindow.destroy, text = "Exit")
+#LEDwindow
+LEDselectexitbutton = guizero.PushButton(LEDwindow, command = LEDwindow.hide, text = "Cancel")
+LEDnameconfirmbutton = guizero.PushButton(LEDwindow, command = updateLEDname, text = "Confirm LED selection")
+
+#user input fields
+LEDnamebox = guizero.TextBox(LEDwindow)
+LEDpinnumberbox = guizero.TextBox(LEDwindow)
+
+#End of file
 mainwindow.display()
 filewrite.close()
+os.exit()
 
